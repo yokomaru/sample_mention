dfd61ab081c4e242ae8bf074e4d7dc91210748e7
yokomaru <cabochetoganlan@gmail.com>
2024-07-07 19:11:44 +0900

    add: relations

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,4 +1,6 @@
 class Mention < ApplicationRecord
-  belongs_to :mentioning_report
-  belongs_to :mentioned_report
+  belongs_to :mentioning_report, class_name: "Report"
+  belongs_to :mentioned_report, class_name: "Report"
+#   belongs_to :from_report, class_name: Report, foreign_key: :mentioning_report_id
+#   belongs_to :mentioned_report, class_name: Report,   foreign_key: :mentioned_report_id
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,9 @@
 class Report < ApplicationRecord
+  has_many :mentioning_report, class_name: "Mention", foreign_key: "mentioning_report_id",  dependent: :destroy # (mentioning_report)
+  has_many :mentioned_report, class_name: "Mention", foreign_key: "mentioned_report_id",  dependent: :destroy # (mentioning_report)
+  # has_many :mentioning_report, class_name: Mention, foreign_key: :mentioning_report_id, dependent: :destroy
+  # has_many :mentioned_report,   class_name: Mention, foreign_key: :mentionined_report_id,   dependent: :destroy
+
+  # has_many :mentioning, through: :mentioning_report, source: :to_user
+  # has_many :mentioned,  through: :mentioned_report,   source: :from_user
 end
